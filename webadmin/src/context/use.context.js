d713f78f945b18e666a871aaa990821e1e5ff5c0
Yuu <105969181+OtosakaYuu203@users.noreply.github.com>
2025-03-15 00:17:49 +0700

    update

--- webadmin/src/context/use.context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { InitState } from "./use.reducer";
-
-const UserContext = createContext([InitState, () => {}]);
-
-export default UserContext;
--- webadmin/src/modules/order/Order.jsx
+++
@@ -1,8 +0,0 @@
-import './Oder.scss';
-const Order = () => {
-    return (
-        <div>Quản lí đơn hàng</div>
-    );
-};
-
-export default Order;
