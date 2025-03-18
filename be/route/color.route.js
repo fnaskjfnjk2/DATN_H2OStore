0a4affbdcb4a830e6ca51d7bdd805553be7ade18
Yuu <105969181+OtosakaYuu203@users.noreply.github.com>
2025-03-19 00:04:19 +0700

    update

--- be/route/color.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const color = require("../controllers/color.controller");
-const authMiddleWare = require("../middleware/auth.middleWare");
-
-router.get("/getColor", color.getColors);
-router.post("/createColor", color.createColor);
-
-module.exports = router;
