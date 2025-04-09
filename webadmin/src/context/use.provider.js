5e54c95a30ee1c1f95551b0b0816548186545787
Yuu <105969181+OtosakaYuu203@users.noreply.github.com>
2025-04-10 00:13:12 +0700

    Update use.provider.js

--- webadmin/src/context/use.provider.js
+++ webadmin/src/context/use.provider.js
@@ -5,10 +5,6 @@ import UserReducer, { InitState } from "./use.reducer";
 function UserProvider({ children }) {
   const [state, dispatch] = useReducer(UserReducer, InitState);
 
-  return (
-    <UserContext.Provider value={[state, dispatch]}>
-      {children}
-    </UserContext.Provider>
-  );
+ 
 }
 export default UserProvider;
