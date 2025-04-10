ea6deb66b0d6389a97b27f477c9c6cc1d93af5ed
Yuu <105969181+OtosakaYuu203@users.noreply.github.com>
2025-04-10 17:58:09 +0700

    note

--- webadmin/src/context/use.provider.js
+++ webadmin/src/context/use.provider.js
@@ -12,3 +12,18 @@ function UserProvider({ children }) {
   );
 }
 export default UserProvider;
+
+// import { useReducer } from "react";
+// import UserContext from "./use.context";
+// import UserReducer, { InitState } from "./use.reducer";
+
+// function UserProvider({ children }) {
+//   const [state, dispatch] = useReducer(UserReducer, InitState);
+
+//   return (
+//     <UserContext.Provider value={[state, dispatch]}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// }
+// export default UserProvider;
--- webadmin/src/context/use.reducer.js
+++ webadmin/src/context/use.reducer.js
@@ -57,3 +57,63 @@ const UserReducer = (state, action) => {
 export { InitState, KEY_CONTEXT_USER };
 
 export default UserReducer;
+
+// import APP_LOCAL from "../lib/localStorage";
+
+// const InitState = {
+//   token: "",
+//   accountType: APP_LOCAL.getAccountTypeStorage(),
+//   isOpenModal: false,
+//   dataModal: null,
+//   isLoading: false,
+//   language: "",
+//   cart: [],
+// };
+// const KEY_CONTEXT_USER = {
+//   SET_TOKEN: "SET_TOKEN",
+//   SET_ACCOUNT_TYPE: "ACCOUNT_TYPE",
+//   SET_LOADING: "SET_LOADING",
+//   CLEAR: "CLEAR",
+//   SHOW_MODAL: "SHOW_MODAL",
+//   HIDE_MODAL: "HIDE_MODAL",
+//   SET_LANGUAGE: "SET_LANGUAGE",
+//   SET_CART: "SET_CART",
+// };
+// const UserReducer = (state, action) => {
+//   switch (action.type) {
+//     case KEY_CONTEXT_USER.SET_TOKEN:
+//       return { ...state, token: action.payload };
+//     case KEY_CONTEXT_USER.SET_ACCOUNT_TYPE:
+//       APP_LOCAL.setAccountTypeStorage(action.payload);
+//       return { ...state, accountType: action.payload };
+//     case KEY_CONTEXT_USER.SET_LOADING:
+//       return { ...state, isLoading: action.payload };
+//     case KEY_CONTEXT_USER.SHOW_MODAL:
+//       return {
+//         ...state,
+//         isOpenModal: true,
+//         titleModel: action.payload.titleModel,
+//         contentModel: action.payload.contentModel,
+//         dataModal: action.payload.dataModal,
+//         typeModal: action.payload.typeModal,
+//         onClickConfirmModel: action.payload.onClickConfirmModel,
+//       };
+//     case KEY_CONTEXT_USER.HIDE_MODAL:
+//       return {
+//         ...state,
+//         isOpenModal: false,
+//         dataModal: null,
+//         onClickConfirmModel: () => {},
+//       };
+//     case KEY_CONTEXT_USER.SET_LANGUAGE:
+//       return { ...state, language: action.payload };
+//     case KEY_CONTEXT_USER.SET_CART:
+//       APP_LOCAL.setCart(JSON.stringify(action.payload));
+//       return { ...state, cart: action.payload };
+//     default:
+//       return state;
+//   }
+// };
+// export { InitState, KEY_CONTEXT_USER };
+
+// export default UserReducer;
