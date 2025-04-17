17c1aedc6344d5c2b9150bf552bbdb0bb11fd77d
Yuu <105969181+OtosakaYuu203@users.noreply.github.com>
2025-04-17 18:24:08 +0700

    Update ErrorPage.jsx

--- webadmin/src/lib/errorpage/ErrorPage.jsx
+++ webadmin/src/lib/errorpage/ErrorPage.jsx
@@ -54,52 +54,52 @@ export default ErrorPage;
 
 
 
-const ErrorPage = () => {
-    const [userCtx, dispatch] = useContext(UserContext)
-    const [isLoading, setIsLoading] = useState(true);
+// const ErrorPage = () => {
+//     const [userCtx, dispatch] = useContext(UserContext)
+//     const [isLoading, setIsLoading] = useState(true);
 
-    const loadingPage = () => {
-        dispatch({ type: KEY_CONTEXT_USER.SET_LOADING, payload: true });
-        setTimeout(() => {
-            dispatch({ type: KEY_CONTEXT_USER.SET_LOADING, payload: false });
-            setIsLoading(false)
-        }, 3000);
-    }
+//     const loadingPage = () => {
+//         dispatch({ type: KEY_CONTEXT_USER.SET_LOADING, payload: true });
+//         setTimeout(() => {
+//             dispatch({ type: KEY_CONTEXT_USER.SET_LOADING, payload: false });
+//             setIsLoading(false)
+//         }, 3000);
+//     }
 
-    useEffect(() => {
-        loadingPage();
-    }, []);
+//     useEffect(() => {
+//         loadingPage();
+//     }, []);
 
-    return (
-        <>
-            {isLoading ? (
-                ""
-            ) : (
-                <div style={styles.container}>
-                    <h1 style={styles.heading}>Error 404</h1>
-                    <p style={styles.message}>Page not found</p>
-                </div>
-            )}
-        </>
-    );
-}
+//     return (
+//         <>
+//             {isLoading ? (
+//                 ""
+//             ) : (
+//                 <div style={styles.container}>
+//                     <h1 style={styles.heading}>Error 404</h1>
+//                     <p style={styles.message}>Page not found</p>
+//                 </div>
+//             )}
+//         </>
+//     );
+// }
 
-const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-    },
-    heading: {
-        fontSize: '3rem',
-        color: '#ff0000',
-    },
-    message: {
-        fontSize: '1.5rem',
-        marginTop: '1rem',
-    },
-};
+// const styles = {
+//     container: {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         height: '100vh',
+//     },
+//     heading: {
+//         fontSize: '3rem',
+//         color: '#ff0000',
+//     },
+//     message: {
+//         fontSize: '1.5rem',
+//         marginTop: '1rem',
+//     },
+// };
 
-export default ErrorPage;
+// export default ErrorPage;
