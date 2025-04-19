010bf915f469e3b2749c2ce142c75fae05a6ae12
Yuu <105969181+OtosakaYuu203@users.noreply.github.com>
2025-04-19 19:49:59 +0700

    Update localStorage.js

--- webadmin/src/lib/localStorage.js
+++ webadmin/src/lib/localStorage.js
@@ -37,39 +37,39 @@ export default APP_LOCAL;
 
 
 
-export const KEY_LOCAL = {
-  TOKEN: "TOKEN",
-  ACCOUNT_TYPE: "ACCOUNT_TYPE",
-  CART: "CART",
-};
-const getTokenStorage = () => {
-  return localStorage.getItem(KEY_LOCAL.TOKEN);
-};
-const setTokenStorage = (token) => {
-  return localStorage.setItem(KEY_LOCAL.TOKEN, token);
-};
-const getAccountTypeStorage = () => {
-  return localStorage.getItem(KEY_LOCAL.ACCOUNT_TYPE);
-};
+// export const KEY_LOCAL = {
+//   TOKEN: "TOKEN",
+//   ACCOUNT_TYPE: "ACCOUNT_TYPE",
+//   CART: "CART",
+// };
+// const getTokenStorage = () => {
+//   return localStorage.getItem(KEY_LOCAL.TOKEN);
+// };
+// const setTokenStorage = (token) => {
+//   return localStorage.setItem(KEY_LOCAL.TOKEN, token);
+// };
+// const getAccountTypeStorage = () => {
+//   return localStorage.getItem(KEY_LOCAL.ACCOUNT_TYPE);
+// };
 
-const setAccountTypeStorage = (type) => {
-  return localStorage.setItem(KEY_LOCAL.ACCOUNT_TYPE, type);
-};
-const getCart = () => {
-  const data = localStorage.getItem(KEY_LOCAL.CART);
-  return JSON.parse(data).filter((i) => i.id);
-};
+// const setAccountTypeStorage = (type) => {
+//   return localStorage.setItem(KEY_LOCAL.ACCOUNT_TYPE, type);
+// };
+// const getCart = () => {
+//   const data = localStorage.getItem(KEY_LOCAL.CART);
+//   return JSON.parse(data).filter((i) => i.id);
+// };
 
-const setCart = (cart) => {
-  return localStorage.setItem(KEY_LOCAL.CART, cart);
-};
-const APP_LOCAL = {
-  getTokenStorage,
-  setTokenStorage,
-  getAccountTypeStorage,
-  setAccountTypeStorage,
-  getCart,
-  setCart,
-};
+// const setCart = (cart) => {
+//   return localStorage.setItem(KEY_LOCAL.CART, cart);
+// };
+// const APP_LOCAL = {
+//   getTokenStorage,
+//   setTokenStorage,
+//   getAccountTypeStorage,
+//   setAccountTypeStorage,
+//   getCart,
+//   setCart,
+// };
 
-export default APP_LOCAL;
+// export default APP_LOCAL;
